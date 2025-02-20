e4c323238c30886e7f3a3354d2eb73e68ecf89e1
Behlül Samil Selcuk <157593831+behluelselcuk@users.noreply.github.com>
2025-02-20 23:50:18 +0100

    Erstelle Eingabe-Elemente
    Die Elemente zu den dazugehörigen Buttons und dem
    Display auf dem Taschenrechenr wurden eingepflegt

---
+++ script.js
@@ -0,0 +1,37 @@
+'use strict'
+
+// Ziel des Algorithmus
+    // Ein Taschenrechner, der alle grundlegenden mathematischen Operatoren enthält, die man so typischerweise kennt
+
+// Eingabe
+    // das Display
+    let display = document.querySelector('.display')
+
+    // erase-Buttons
+    let clearBtn = document.querySelector('.clear')
+    let deleteBtn = document.querySelector('.delete')
+
+    // number-Buttons
+    let zeroBtn = document.querySelector('.zero')
+    let oneBtn = document.querySelector('.one')
+    let twoBtn = document.querySelector('.two')
+    let threeBtn = document.querySelector('.three')
+    let fourBtn = document.querySelector('.four')
+    let fiveBtn = document.querySelector('.five')
+    let sixBtn = document.querySelector('.six')
+    let sevenBtn = document.querySelector('.seven')
+    let eightBtn = document.querySelector('.eight')
+    let nineBtn = document.querySelector('.nine')
+
+    // operater-Buttons
+    let divideBtn = document.querySelector('.divide')
+    let multiplyBtn = document.querySelector('.multiply')
+    let minusBtn = document.querySelector('.minus')
+    let plusBtn = document.querySelector('.plus')
+    let euqalBtn = document.querySelector('.equal')
+    let commaBtn = document.querySelector('.comma')
+
+
+// Verarbeitung
+
+// Ausgabe
