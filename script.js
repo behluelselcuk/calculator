cb70887abc5dddf8950cc172bde4f5669136bc42
Behlül Samil Selcuk <157593831+behluelselcuk@users.noreply.github.com>
2025-02-22 12:31:24 +0100

    Vervollständige erase-Button-Funktionen

--- script.js
+++ script.js
@@ -13,8 +13,8 @@
 
     // Button-Klasse
     let btns = document.querySelectorAll('.buttons')
-    let numberBtns = document.querySelector('.numberButtons')
-    let operatorBtns = document.querySelector('.operatorButtons')
+    let numberBtns = document.querySelectorAll('.numberButtons')
+    let operatorBtns = document.querySelectorAll('.operatorButtons')
 
     // number-Buttons
     let zeroBtn = document.querySelector('#zero')
@@ -38,12 +38,12 @@
 
 
 // Verarbeitung
-    // beim Betätigen einer number-Button (einschließlich Komma-Button), soll die Anzeige auf dem Display sich dementsprechend ändern
+    // beim Betätigen einer number-Button, soll die Anzeige auf dem Display sich dementsprechend ändern
     // beim Betätigen der erase-Buttons, soll das Display dementsprechend (delete / clear) gereinigt werden
-    // beim Betätigen der operator-buttons, soll sich das display dementsprechend anpassen und die Funktion hinter der jeweiligen operators ausführen
+    // beim Betätigen der operator-buttons (einschließlich Komma-Button), soll sich das display dementsprechend anpassen und die Funktion hinter der jeweiligen operators ausführen
     // die Funktionen hinter den operator-Buttons einpflegen
 
-function showButtonValue(event) {
+function showNumberButtonValue(event) {
     const value = event.target.value;
 
     if (display.textContent === '0') {
@@ -52,15 +52,71 @@ function showButtonValue(event) {
     else {
         display.textContent += value;
     }
-    
 }
 
-btns.forEach(btn => {
-    btn.addEventListener('click', showButtonValue)
-});
+function showOperatorButtonValue(event) {
+    const value = event.target.value;
+
+    if (display.textContent !== '0') {
+        display.textContent += ` ${value} `;
+    }
+}
+
+function clearDisplay() {
+    display.textContent = 0;
+}
+
+function deleteDisplay() {
+    // überprüft, ob etwas außer 0
+        // falls ja => entferne letztes Zeichen, wenn nur noch ein Zeichen, entferne dieses und mach eine Null hin
+            // überprüfe, ob Zahl oder Operator
+                // falls Operator (außer Komma) => lösche drei Zeichen
+                // falls Zahl (einschließlich Komma) => lösche ein Zeichen
+        // falls nein => bleibt gleich
+    let content = display.textContent;
+
+    if (content.length <= 1) {
+        display.textContent = '0';
+    }
+    else if (content.length > 1) {
+        if (content.endsWith(' ')) {
+            display.textContent = display.textContent.slice(0, -3);
+        }
+        else {
+            display.textContent = display.textContent.slice(0, -1);
+        }
+    }
+}
+
+function add(x, y) {
+    return x + y;
+}
+
+function minus(x, y) {
+    return x - y;
+}
+
+function multiply(x, y) {
+    return x * y;
+}
+
+function divide(x, y) {
+    return x / y;
+}
 
 
 
+numberBtns.forEach(btn => {
+    btn.addEventListener('click', showNumberButtonValue)
+})
+
+operatorBtns.forEach(btn => {
+    btn.addEventListener('click', showOperatorButtonValue)
+})
+
+clearBtn.addEventListener('click', clearDisplay)
+
+deleteBtn.addEventListener('click', deleteDisplay)
 
 
 
